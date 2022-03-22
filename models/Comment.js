66e0b32867eaf88d3df4d1268235a1ca52c7a2c7
Jonathan W <jwilliams12117@gmail.com>
2022-03-21 19:04:49 -0700

    Added comment model

---
+++ models/Comment.js
@@ -0,0 +1,39 @@
+const {Model, DataTypes} = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            allowNull: false
+        },
+        comment_text: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "user"
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "post"
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "comment"
+    }
+);
+
+module.exports = Comment;
