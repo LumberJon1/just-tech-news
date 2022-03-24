362acbeee2c162744f288285eddd150fcfbe06bb
Jonathan W <jwilliams12117@gmail.com>
2022-03-23 21:18:13 -0700

    Added logout route

--- controller/api/user-routes.js
+++ controller/api/user-routes.js
@@ -96,7 +96,19 @@ router.post("/login", (req, res) => {
             res.json({user: dbUserData, message: "You are now logged in."});
         });
     })
-})
+});
+
+// Logout POST route
+router.post("/logout", (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
