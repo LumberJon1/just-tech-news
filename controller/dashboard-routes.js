bea683a58a1854bfd672d17e3507e724a995b67e
Jonathan W <jwilliams12117@gmail.com>
2022-03-25 14:43:59 -0700

    Added post display to dashboard and form submission to create new posts

--- controller/dashboard-routes.js
+++ controller/dashboard-routes.js
@@ -4,7 +4,41 @@ const { Post, User, Comment } = require('../models');
 
 // Dashboard landing page GET route
 router.get("/", (req, res) => {
-    res.render("dashboard", {loggedIn: true});
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            "id",
+            "post_url",
+            "title",
+            "created_at",
+            [sequelize.literal("(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"), "vote_count"]
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                model: User,
+                attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ["username"]
+            }
+        ]
+    })
+    .then(dbPostData => {
+        // Serialize for use in the res.render function
+        const posts = dbPostData.map(post => post.get({plain: true}));
+        res.render("dashboard", {posts, loggedIn: true});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
