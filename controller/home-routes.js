f990a5e8ef6ef7b228fa092446a551f89ea2d898
Jonathan W <jwilliams12117@gmail.com>
2022-03-24 17:55:14 -0700

    added upvote capability for post:

--- controller/home-routes.js
+++ controller/home-routes.js
@@ -38,6 +38,51 @@ router.get('/', (req, res) => {
       });
   });
 
+// Single post route
+router.get("/post/:id", (req, res) => {
+
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            "id",
+            "post_url",
+            "title",
+            "created_at",
+            [sequelize.literal("(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"), "vote_count"]
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+                include: {
+                    model: User,
+                    attributes: ["username"]
+                }
+            },
+            {
+                model: User,
+                attributes: ["username"]
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({message: "No post found with this ID"});
+            return;
+        }
+        // Don't forget to serialize post data so Handlebars can read it
+        const post = dbPostData.get({plain: true});
+
+        res.render("single-post", {post});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 // Route to go to signup page
 router.get("/login", (req, res) => {
     if (req.session.loggedIn) {
